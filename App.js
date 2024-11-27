1a6152a4af723bc4a8be37d6e1c1df636a4ea7c8
Kari <kari.lynn.dreyer@gmail.com>
2024-11-27 09:16:21 +0100

    Develop main UI component

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+// Iitialize connection for Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Import the screens
+import ShoppingLists from './components/ShoppingLists';
+import Welcome from './components/Welcome';
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBM3wUtp2R1ncBGsqZC3eZoJjMwm2VGr9s",
+    authDomain: "shopping-list-demo-66412.firebaseapp.com",
+    projectId: "shopping-list-demo-66412",
+    storageBucket: "shopping-list-demo-66412.firebasestorage.app",
+    messagingSenderId: "741231738101",
+    appId: "1:741231738101:web:443682902afb23e88519bc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen 
+          name="Welcome" 
+          component={Welcome} 
+        />
+        <Stack.Screen
+          name="ShoppingLists"
+        >
+         {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
