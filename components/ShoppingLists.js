18b77a4b8cbdcf3c5e61081c1a37bc98870f0e6c
Kari <kari.lynn.dreyer@gmail.com>
2024-12-05 20:15:38 +0100

    Feat: Allow for offline list storage using AsyncStorage

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,8 +1,9 @@
 import { Alert, FlatList, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { addDoc, collection, getDocs, onSnapshot, query, where } from "firebase/firestore";
 
-const ShoppingLists = ({ db, route }) => {
+const ShoppingLists = ({ db, route, isConnected }) => {
     const { userID } = route.params;
 
     const [lists, setLists] = useState([]);
@@ -29,21 +30,45 @@ const ShoppingLists = ({ db, route }) => {
         }
     }
 
+    let unsubShoppinglists;
+
     useEffect(() => {
-        const q = query(collection(db, "shoppinglists"), where("uid", "==", userID));
-        const unsubShoppinglists = onSnapshot(q, (documentsSnapshot) => {
-            let newLists = [];
-            documentsSnapshot.forEach(doc => {
-                newLists.push({ id: doc.id, ...doc.data() })
+        
+        if (isConnected === true) {
+
+            // unregister current onSnapshot() listener to avoid registering multiple listeners when useEffect code is re-executed
+            if (unsubShoppinglists) unsubShoppinglists();
+            unsubShoppinglists = null;
+
+            const q = query(collection(db, "shoppinglists"), where("uid", "==", userID));
+            unsubShoppinglists = onSnapshot(q, (documentsSnapshot) => {
+                let newLists = [];
+                documentsSnapshot.forEach(doc => {
+                    newLists.push({ id: doc.id, ...doc.data() })
+                });
+                cacheShoppingLists(newLists);
+                setLists(newLists);
             });
-            setLists(newLists);
-        });
+        } else loadCachedLists();
 
         // Clean up code
         return () => {
             if (unsubShoppinglists) unsubShoppinglists();
         }
-    }, []);
+    }, [isConnected]);
+
+    const cacheShoppingLists = async (listsToCache) => {
+        try {
+            await AsyncStorage.setItem('shopping_lists', JSON.stringify(listsToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
+      }
+    
+    const loadCachedLists = async () => {
+        const cachedLists = await AsyncStorage.getItem("shopping_lists") || [];
+        setLists(JSON.parse(cachedLists));
+    }
 
     return (
         <View style={styles.container}>
@@ -56,47 +81,67 @@ const ShoppingLists = ({ db, route }) => {
                     </View>
                 }
             />
-            <View>
-                <TextInput 
-                    style={styles.listName}
-                    placeholder="List Name"
-                    value={listName}
-                    onChangeText={setListName}
-                />
-                <TextInput 
-                    style={styles.item}
-                    placeholder="Item #1"
-                    value={item1}
-                    onChangeText={setItem1}
-                />
-                <TextInput 
-                    style={styles.item}
-                    placeholder="Item #2"
-                    value={item2}
-                    onChangeText={setItem2}
-                />
-                <TouchableOpacity
-                    style={styles.addButton}
-                    onPress={() => {
-                        const newList = {
-                            uid: userID,
-                            name: listName,
-                            items: [item1, item2]
-                        }
-                        addShoppingList(newList);
-                    }}
-                >
-                <Text style={styles.addButtonText}>Add</Text>
-                </TouchableOpacity>
-            </View>
+            {(isConnected === true) ?
+                <View style={styles.formContainer}>
+                    <TextInput 
+                        style={styles.listName}
+                        placeholder="List Name"
+                        value={listName}
+                        onChangeText={setListName}
+                    />
+                    <TextInput 
+                        style={styles.item}
+                        placeholder="Item #1"
+                        value={item1}
+                        onChangeText={setItem1}
+                    />
+                    <TextInput 
+                        style={styles.item}
+                        placeholder="Item #2"
+                        value={item2}
+                        onChangeText={setItem2}
+                    />
+                    <TouchableOpacity
+                        style={styles.addButton}
+                        onPress={() => {
+                            const newList = {
+                                uid: userID,
+                                name: listName,
+                                items: [item1, item2]
+                            }
+                            addShoppingList(newList);
+                        }}
+                    >
+                    <Text style={styles.addButtonText}>Add</Text>
+                    </TouchableOpacity>
+                </View> : null
+             }
             {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    addButton: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        backgroundColor: "#000",
+        color: "#FFF"
+    },
+    addButtonText: {
+        color: "#FFF",
+        fontWeight: "600",
+        fontSize: 20
+    },
     container: {
       flex: 1
+    },
+    formContainer: {
+        backgroundColor: "#999",
+        margin: 15,
+        padding: 15,
+        borderRadius: 8,
     },
     listItem: {
         height: 70,
@@ -130,18 +175,6 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         borderColor: "#555",
         borderWidth: 2
-    },
-    addButton: {
-        justifyContent: "center",
-        alignItems: "center",
-        height: 50,
-        backgroundColor: "#000",
-        color: "#FFF"
-    },
-    addButtonText: {
-        color: "#FFF",
-        fontWeight: "600",
-        fontSize: 20
     }
 });
 
